7f2dedd6e375a1326c2840a040ea56b1a41ae14b
eilonaharoni <eaharoni88@gmail.com>
2024-09-02 11:41:07 +0300

    add Crew Member Class

--- CCrewMember.cpp
+++ CCrewMember.cpp
@@ -1,5 +1,65 @@
 //
 // Created by אילון אהרוני on 01/09/2024.
 //
-
+#include <iostream>
 #include "CCrewMember.h"
+
+
+
+CCrewMember::CCrewMember(const string& name,const CAddress& newAddress, int airTime) {
+    this->name = name;
+    this->address = new CAddress(newAddress);
+    this->airTime = airTime;
+
+
+}
+
+bool CCrewMember::UpdateMinutes(int minuets) {
+    if (minuets + this->airTime >= 0)
+    {
+        this->airTime+=minuets;
+        return true;
+    }
+    return false;
+
+}
+
+int CCrewMember::getAirTime() const {
+    return this->airTime;
+}
+
+void CCrewMember::setName(string &newName) {
+    this-> name = newName;
+
+}
+
+string CCrewMember::getName() const{
+    return this->name;
+}
+
+void CCrewMember::setAddress(CAddress &newAddress) {
+    this->address->updateAddress(newAddress.getCity(),newAddress.getStreet(), newAddress.getHouseNumber());
+
+}
+
+CAddress CCrewMember::getAddress() {
+    return *this->address;
+}
+
+void CCrewMember::print() {
+    cout << "Name:" << this->name << ", " << "Air Time(Minutes):" << this->airTime << " , ";
+    this->address->print();
+
+
+}
+
+bool CCrewMember::IsEqual(CCrewMember &other) {
+    return this->name == other.name;
+}
+
+CCrewMember::~CCrewMember() {
+    delete address;
+    address = nullptr;
+}
+
+
--- CCrewMember.h
+++ CCrewMember.h
@@ -4,10 +4,34 @@
 
 #ifndef AIRPORT_CCREWMEMBER_H
 #define AIRPORT_CCREWMEMBER_H
+#include "CAddress.h"
+#include <string>
 
+using namespace std;
 
 class CCrewMember {
+private:
+    string name;
+    int airTime; // in minutes
+    CAddress* address;
 
+public:
+    CCrewMember(const string& name,const CAddress& address,int airTime = 0);
+
+    bool UpdateMinutes(int minuets);
+    int getAirTime() const;
+
+    void setName(string& newName);
+    string getName() const;
+
+    void setAddress(CAddress& newAddress);
+    CAddress getAddress() ;
+
+    void print();
+
+    bool IsEqual(CCrewMember& other);
+
+    ~CCrewMember();
 };
 
 
