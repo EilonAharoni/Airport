6b3e08f957302d6338c26a8d97a0c431ccc7a72f
eilonaharoni <eaharoni88@gmail.com>
2024-09-12 18:23:22 +0300

    add CFlight Calss

---
+++ CFlight.cpp
@@ -0,0 +1,5 @@
+//
+// Created by אילון אהרוני on 12/09/2024.
+//
+
+#include "CFlight.h"
---
+++ CFlight.h
@@ -0,0 +1,20 @@
+//
+// Created by אילון אהרוני on 12/09/2024.
+//
+
+#ifndef AIRPORT_CFLIGHT_H
+#define AIRPORT_CFLIGHT_H
+#include "CFlightInfo.h"
+#include "CPlane.h"
+class CFlight {
+private:
+
+
+
+
+public:
+
+};
+
+
+#endif //AIRPORT_CFLIGHT_H
