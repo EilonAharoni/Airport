29077d0d3295b7e5843cebc56eb2c64b2295e64d
Erez Lavi <Erez.Lavi@s.afeka.ac.il>
2024-09-22 15:30:46 +0300

    -New class added: CCargo.

---
+++ Cargo.cpp
@@ -0,0 +1,33 @@
+#include "Cargo.h"
+
+CCargo::CCargo(int numOfSeats, string modelName, const float maxCargoWeight, const float maxCargoVolume) : CPlane(numOfSeats, modelName),
+maxCargoWeight(maxCargoWeight), maxCargoVolume(maxCargoVolume), currentCargoWeight(0), currentCargoVolume(0)
+{
+}
+
+bool CCargo::load(float volume, float weight)
+{
+	float currentWeight = this->currentCargoWeight;
+	float currentVolume = this->currentCargoVolume;
+	float maxWeight = this->maxCargoWeight;
+	float maxVolume = this->maxCargoVolume;
+	if (currentVolume + volume <= maxVolume && currentWeight + weight <= maxWeight)
+	{
+		this->currentCargoVolume += volume;
+		this->currentCargoWeight += weight;
+		return true;
+	}
+	return false;
+}
+
+void CCargo::takeOff(int minutes) const
+{
+	cout << "Need to add " << minutes << " minutes to my log book" << endl;
+}
+
+void CCargo::print(ostream& out) const
+{
+	CPlane::print();
+	cout <<"Cargo Weight: " << this->currentCargoWeight << endl << "Cargo Volume: " << this->currentCargoVolume << endl;
+}
+
